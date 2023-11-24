d146cb3a34dc9e74c183daf6720b91bd302dd7b0
mohd aamir <venomaamir@gmail.com>
2023-11-24 23:43:06 +0530

    added a new seach Page java file in main source folder

---
+++ src/main/java/com/amazon/qa/pages/SearchPage.java
@@ -0,0 +1,8 @@
+package com.amazon.qa.pages;
+
+public class SearchPage {
+    private void searchPag() {
+		// TODO Auto-generated method stub
+        System.out.println("search page clas is executed");
+	}
+}
